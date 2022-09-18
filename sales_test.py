7c6709752a2d3ef46eb73fc92d30d823143a4f8c
benjaminbills <benjaminoba234@gmail.com>
2022-09-18 21:42:15 +0300

    test tax exempt

--- sales_test.py
+++ sales_test.py
@@ -1,5 +1,6 @@
 import unittest
 from sales import Sales
+from run import check_basic_tax_exempt
 
 class TestSales(unittest.TestCase):
   def setUp(self):
@@ -17,6 +18,11 @@ class TestSales(unittest.TestCase):
     product_details_2 = Sales('1 music CD', 14.99)
     product_details_2.save_item()
     self.assertEqual(len(Sales.display_sales()), 2)
+
+  def test_check_basic_tax_exempt(self):
+    tax_exempt_item = Sales('1 chocolate bar', 0.85)
+    check_exempt = check_basic_tax_exempt(tax_exempt_item.product_name)
+    self.assertEqual(check_exempt,True)
 
 if __name__ == '__main__':
     unittest.main()
