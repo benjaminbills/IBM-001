bfe63230d495fe2617cfcc44a17a446e1f6e42c4
benjaminbills <benjaminoba234@gmail.com>
2022-09-18 21:14:52 +0300

    set up save items

--- sales.py
+++ sales.py
@@ -1,7 +1,13 @@
 class Sales:
   sales_list = []
-  calculated_product= []
   
   def __init__(self,product_name,price):
     self.product_name = product_name
     self.price = price
+
+  def save_item(self):
+    Sales.sales_list.append(self)
+    
+  @classmethod
+  def display_sales(products):
+    return products.sales_list
